540fce4025292bf05723ff43928e786fac8a506b
Guilherme Hastenreiter Ferreira <hast.job@gmail.com>
2024-08-13 12:43:25 -0300

    Feat: Add email get API

--- src/emails.php
+++ src/emails.php
@@ -2,24 +2,36 @@
 
 require_once("Controllers/Controler.php");
 require_once("tokenLib.php");
+require_once("Database/Connection.php");
+require_once("Services/Service.php");
 
 // Class definition
 class EmailController extends Controller {
+	private $service;
+
+	function __construct($guards, $service) {
+		$this->service = $service;
+		parent::__construct($guards);
+	}
+
 	function r_getOne($id) {
-		return array(
-			"message" => "This should return one email with id: $id.",
-		);
+		return $this->service->getOne($id);
 	}
 
 	function r_getAll() {
-		return array(
-			"message" => "This should return all emails.",
-		);
+		if ( isset($_GET['limit']) ){
+			return (
+				!isset($_GET['offset']) ?
+				$this->service->getAll($_GET['limit']) :
+				$this->service->getAll($_GET['limit'], $_GET['offset'])
+			);
+		}
+		return $this->service->getAll();
 	}
 
 	function r_post() {
-		array(
-			"message" => "This should create a new email.",
+		return $this->service->create(
+				(array)json_decode($_POST['data'])
 		);
 	}
 
@@ -39,13 +51,30 @@ class EmailController extends Controller {
 
 // Setting up guards
 $guards = array(
-	"Login required"=> function (){
+	"Login required" => function () {
 		return isLogged();
 	}
 );
+$emailsTable = "successful_emails";
+$emailsModel = array(
+	'id' => PDO::PARAM_INT,
+	'affiliate_id' => PDO::PARAM_INT,
+	'envelope' => PDO::PARAM_STR,
+	'from' => PDO::PARAM_STR,
+	'subject' => PDO::PARAM_STR,
+	'dkim' => PDO::PARAM_STR,
+	'SPF' => PDO::PARAM_STR,
+	'spam_score' => PDO::PARAM_STR,
+	'email' => PDO::PARAM_STR,
+	'raw_text' => PDO::PARAM_STR,
+	'sender_ip' => PDO::PARAM_STR,
+	'to' => PDO::PARAM_STR,
+	'timestamp' => PDO::PARAM_INT,
+);
+
+$db = MysqlPDO::fromEnv();
+$emailService = new Service($db, $emailsTable, $emailsModel);
 
 // Handling the requests
-$emailController = new EmailController($guards);
+$emailController = new EmailController($guards, $emailService);
 $emailController->handleRequest();
-
-?>
